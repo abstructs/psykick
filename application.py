f277603b29313edc5d7cc73be409e019ef0954fe
Christina Lee <christinalee@v1022-rn-217-173.campus-dynamic.uwaterloo.ca>
2017-12-02 12:14:47 -0500

    flask routing

--- application.py
+++ application.py
@@ -2,12 +2,13 @@
 from flask import Flask, request, render_template
 import sqlite3
 
-# DATABASE = 
+# DATABASE =
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # return render_template('index.html')
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
