0040174e85433994f1234caaf6dada58e683d3bd
abstructs <andrewclarke540@gmail.com>
2017-12-02 12:53:11 -0500

    stufffssf

--- application.py
+++ application.py
@@ -2,11 +2,16 @@
 from flask import Flask, request, render_template
 import sqlite3
 
-# DATABASE = 
+DATABASE = 'psykick.db'
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+    return 'hello'
     # return render_template('index.html')
 
 if __name__ == "__main__":
