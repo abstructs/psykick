a6862351ca408c690388dc4d5a0131a8f09e40c7
abstructs <andrewclarke540@gmail.com>
2017-12-02 12:08:12 -0500

    stuff

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
 
-from flask import Flask
+from flask import Flask, request, render_template
+import sqlite3
+
+# DATABASE = 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    # return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
